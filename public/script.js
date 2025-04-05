f6ccb6fa331dc1610badd153cf32ca5922bcaf55
ejg21 <161253173+ejg21@users.noreply.github.com>
2025-04-04 20:51:34 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,24 +1,23 @@
 function searchVideo() {
+  console.log("it working");
   const videoUrl = document.getElementById("userInput").value;
-  console.log('Video URL:', videoUrl);  // Debugging line
+  
   fetch(`/videoInfo/query?url=${videoUrl}`)
     .then((response) => response.json())
     .then((data) => {
-      if (data.success) {
-        const ytThumbnail = data.data.ytThumbnail;
-        let downloadBox = document.getElementsByClassName("downloadBox")[0];
-        let downOption = `
-          <img class="thumbnailImage" src="${ytThumbnail}" alt="example">
-          <div class="buttonInfo">
-              <button class="searchButton downloadButton" onclick="getAudio()">audio</button>
-              <button class="searchButton downloadButton" onclick="getVideo()">video</button>
-              <button class="searchButton downloadButton" onclick="getBoth()">download</button>
-          </div>
-        `;
-        downloadBox.innerHTML = downOption;
-      } else {
-        console.error("Error:", data.message);
-      }
+      // Process the returned JSON data
+      const ytThumbnail = data.data.ytThumbnail;
+      let downloadBox = document.getElementsByClassName("downloadBox")[0];
+      let downOption = `
+        <img class="thumbnailImage" src="${ytThumbnail}" alt="example">
+        <div class="buttonInfo">
+            <button class="searchButton downloadButton" onclick="getAudio()">audio</button>
+            <button class="searchButton downloadButton" onclick="getVideo()">video</button>
+            <button class="searchButton downloadButton" onclick="getBoth()">download</button>
+        </div>
+      `;
+      downloadBox.innerHTML = downOption;
+      // Do something with the data
     })
     .catch((error) => {
       console.error("Error:", error);
@@ -31,8 +30,8 @@ function getAudio() {
 }
 
 function getVideo() {
-  alert("Check your download folder!!!");
-  fetch(`/options/mp4`);
+  const videoUrl = document.getElementById("userInput").value;
+  window.location.href = `/video/${encodeURIComponent(videoUrl)}`;
 }
 
 function getBoth() {
