0edbbb8eb0486e17eb5f34a51a0792b5cd91b39c
ejg21 <161253173+ejg21@users.noreply.github.com>
2025-04-04 20:55:50 -0400

    Update ytdl.js

--- ytdl.js
+++ ytdl.js
@@ -1,5 +1,15 @@
+const ytdl = require("ytdl-core");
 const fs = require("fs");
 const path = require("path");
+
+const getVideoInfo = (videoUrl) => {
+  const videoId = ytdl.getVideoID(videoUrl);
+  const ytThumbnail = {
+    ytThumbnail: "https://img.youtube.com/vi/" + videoId + "/sddefault.jpg",
+    videoId: videoId,
+  };
+  return ytThumbnail;
+};
 
 const download = (videoId, filter, quality, fileFormat) => {
   // Use the current directory (.) for downloads
