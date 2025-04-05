d15827616fd9326018a26d5348185831fa9f5e36
ejg21 <161253173+ejg21@users.noreply.github.com>
2025-04-04 20:47:34 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,22 +1,24 @@
 function searchVideo() {
-  console.log("it working");
   const videoUrl = document.getElementById("userInput").value;
+  console.log('Video URL:', videoUrl);  // Debugging line
   fetch(`/videoInfo/query?url=${videoUrl}`)
     .then((response) => response.json())
     .then((data) => {
-      // Process the returned JSON data
-      const ytThumbnail = data.data.ytThumbnail;
-      let downloadBox = document.getElementsByClassName("downloadBox")[0];
-      let downOption = `
-        <img class="thumbnailImage" src="${ytThumbnail}" alt="example">
-        <div class="buttonInfo">
-            <button class="searchButton downloadButton" onclick="getAudio()">audio</button>
-            <button class="searchButton downloadButton" onclick="getVideo()">video</button>
-            <button class="searchButton downloadButton" onclick="getBoth()">download</button>
-        </div>
-      `;
-      downloadBox.innerHTML = downOption;
-      // Do something with the data
+      if (data.success) {
+        const ytThumbnail = data.data.ytThumbnail;
+        let downloadBox = document.getElementsByClassName("downloadBox")[0];
+        let downOption = `
+          <img class="thumbnailImage" src="${ytThumbnail}" alt="example">
+          <div class="buttonInfo">
+              <button class="searchButton downloadButton" onclick="getAudio()">audio</button>
+              <button class="searchButton downloadButton" onclick="getVideo()">video</button>
+              <button class="searchButton downloadButton" onclick="getBoth()">download</button>
+          </div>
+        `;
+        downloadBox.innerHTML = downOption;
+      } else {
+        console.error("Error:", data.message);
+      }
     })
     .catch((error) => {
       console.error("Error:", error);
@@ -27,10 +29,12 @@ function getAudio() {
   alert("Check your download folder!!!");
   fetch(`/options/mp3`);
 }
+
 function getVideo() {
   alert("Check your download folder!!!");
   fetch(`/options/mp4`);
 }
+
 function getBoth() {
   alert("Check your download folder!!!");
   fetch(`/options/both`);
